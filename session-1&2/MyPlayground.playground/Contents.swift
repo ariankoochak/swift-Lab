07abb772360f6301f25dc1900f0fc444d14aa623
Arian <arian.koochak@protonmail.com>
2024-07-25 11:45:52 +0330

    add first task

---
+++ session-1&2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+var students:[[String : String]] = [
+    [
+        "first-name" : "Arian",
+        "last-name" : "Koochak",
+        "score" : "25",
+    ],
+    [
+        "first-name" : "Abbas",
+        "last-name" : "Elahi",
+        "score" : "35",
+    ],
+    [
+        "first-name" : "Hossein",
+        "last-name" : "Elahi",
+        "score" : "30",
+    ],
+]
+
+print(students[0]["score"]!)
